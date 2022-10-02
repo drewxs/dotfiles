d0badb5dfcddda55b149558f97baa4eeae795e76
Andrew X. Shah <drewshah0@gmail.com>
2022-10-02 16:40:25 -0600

    feat: add .p10k.zsh

--- scripts/create_symlinks.sh
+++ scripts/create_symlinks.sh
@@ -2,4 +2,5 @@
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/zsh/.zsh_aliases ~/.zsh_aliases
+ln -s ~/.dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
 ln -s ~/.dotfiles/nvim ~/.config/nvim
