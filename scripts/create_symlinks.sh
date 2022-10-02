425ed1be4462cec2cc493196146ac15fd035a166
Andrew X. Shah <drewshah0@gmail.com>
2022-10-02 16:47:18 -0600

    feat: add .zshrc

--- scripts/create_symlinks.sh
+++ scripts/create_symlinks.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/zsh/.zsh_aliases ~/.zsh_aliases
 ln -s ~/.dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
 ln -s ~/.dotfiles/nvim ~/.config/nvim
