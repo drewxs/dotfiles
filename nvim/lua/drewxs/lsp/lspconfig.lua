7c997a67e1ebb89f18a89facaa8a2b431bf19cdd
Andrew X. Shah <drewshah0@gmail.com>
2022-10-15 03:14:48 -0600

    fix: resolve update_capabilities deprecation in favor of default_capabilities

--- nvim/lua/drewxs/lsp/lspconfig.lua
+++ nvim/lua/drewxs/lsp/lspconfig.lua
@@ -34,7 +34,7 @@ protocol.CompletionItemKind = {
 }
 
 -- set up completion using nvim_cmp with LSP source
-local capabilities = require('cmp_nvim_lsp').update_capabilities(
+local capabilities = require('cmp_nvim_lsp').default_capabilities(
   vim.lsp.protocol.make_client_capabilities()
 )
 
@@ -97,5 +97,3 @@ lspconfig.eslint.setup({
     },
   },
 })
-
-
