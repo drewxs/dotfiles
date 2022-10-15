3ebc64ed62fe323f4b63991230503c451b741119
Andrew X. Shah <drewshah0@gmail.com>
2022-10-15 03:15:06 -0600

    refactor: install scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,58 +1,66 @@
 #!/bin/sh
 
-# start at root
-cd ~
+function install_apt_packages() {
+  sudo apt-get install -y zsh ripgrep fuse libfuse2 ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+}
 
-# apt packages
-apt=get install -y zsh ripgrep fuse libfuse2 ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+function install_cmake() {
+  cd ~
+  wget https://github.com/Kitware/CMake/releases/download/v3.24.2/cmake-3.24.2.tar.gz
+  tar -xf cmake-3.24.2.tar.gz
+  cd cmake-3.24.2
+  ./bootstrap
+  make
+  cd ~
+  rm -rf cmake-3.24.2 cmake-3.24.2.tar.gz
+}
 
-# cmake
-wget https://github.com/Kitware/CMake/releases/download/v3.24.2/cmake-3.24.2.tar.gz
-tar -xf cmake-3.24.2.tar.gz
-cd cmake-3.24.2
-./bootstrap
-make
-cd ~
+function install_packer() {
+  git clone --depth 1 https://github.com/wbthomason/packer.nvim \
+    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+}
 
-# zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+function install_lazygit() {
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[0-35.]+')
+  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+  tar -xf lazygit.tar.gz -C /usr/local/bin lazygit
+}
 
-# packer
-git clone --depth 1 https://github.com/wbthomason/packer.nvim \
-  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
-# lazygit
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[0-35.]+')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar -xf lazygit.tar.gz -C /usr/local/bin lazygit
-
-# nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-nvm install 16
+function install_node() {
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+  nvm install 16
+}
 
 # npm packages
-npm i -g pnpm @fsouza/prettierd eslint_d typescript-language-server
+function install_npm_packages() {
+  npm i -g pnpm @fsouza/prettierd eslint_d typescript-language-server
+}
 
 # neovim
-git clone https://github.com/neovim/neovim
-cd neovim
-git checkout stable
-make CMAKE_BUILD_TYPE=Release
-make install
-cd ~
+function install_neovim() {
+  git clone https://github.com/neovim/neovim
+  cd neovim
+  git checkout stable
+  make CMAKE_BUILD_TYPE=Release
+  sudo make install
+  cd ~
+  rm -rf neovim
+}
+
+install_apt_packages
+install_cmake
+install_packer
+install_lazygit
+install_node
+install_npm_packages
+install_neovim
 
 # remove existing configurations
 rm -rf .gitconfig .tmux.conf .zshrc .p10k.zsh .config/nvim
 
-# create symlinks from .dotfiles
+# create symlinks wrom .dotfiles
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
 ln -s ~/.dotfiles/nvim ~/.config/nvim
-
-# neovim packages
-nvim :PackerSync
