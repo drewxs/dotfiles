f9896b426b7184fdd47bc003444a8de63ae38b25
Andrew X. Shah <drewshah0@gmail.com>
2022-10-22 02:54:43 -0600

    feat: update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -33,7 +33,7 @@ function install_cmake() {
 }
 
 function install_lazygit() {
-  [[ -d lazygit.tar.gz ]] sudo rm -rf lazygit.tar.gz
+  sudo rm -rf lazygit.tar.gz
   LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[0-35.]+')
   curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
   tar -xf lazygit.tar.gz -C /usr/local/bin lazygit
