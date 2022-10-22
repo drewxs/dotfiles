03b5f191300acd17459a77f2277d0a145ef15561
Andrew X. Shah <drewshah0@gmail.com>
2022-10-22 02:43:52 -0600

    feat: update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -22,18 +22,18 @@ function install_zsh() {
 
 function install_cmake() {
   cd ~
-  sudo rm -rf cmake-3.24.2 cmake-3.24.2.tar.gz
+  sudo rm -rf cmake-3*
   wget https://github.com/Kitware/CMake/releases/download/v3.24.2/cmake-3.24.2.tar.gz
   tar -xf cmake-3.24.2.tar.gz
   cd cmake-3.24.2
   ./bootstrap
   make
-  sudo rm -rf cmake-3.24.2 cmake-3.24.2.tar.gz
+  sudo rm -rf cmake-3*
   cd ~
 }
 
 function install_lazygit() {
-  sudo rm -rf lazygit.tar.gz
+  [[ -d lazygit.tar.gz ]] sudo rm -rf lazygit.tar.gz
   LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[0-35.]+')
   curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
   tar -xf lazygit.tar.gz -C /usr/local/bin lazygit
@@ -42,37 +42,24 @@ function install_lazygit() {
 
 function install_rust() {
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-}
-
-function install_cargo_packages() {
+  1
   cargo install stylua
 }
 
 function install_node() {
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
   nvm install 16
+  npm i -g neovim pnpm @fsouza/prettierd eslint_d typescript-language-server
 }
 
-function install_npm_packages() {
-  npm i -g pnpm @fsouza/prettierd eslint_d typescript-language-server
+function install_ruby() {
+  sudo apt-get install -y ruby-full
+  sudo gem install neovim
 }
 
-function install_python() {
-  sudo rm -rf Python-3.10.8 Python-3.10.8.tgz
-  libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
-  wget https://www.python.org/ftp/python/3.10.8/Python-3.10.8.tgz
-  tar -xf Python-3.10.8.tgz
-  cd Python-3.10.8
-  ./configure --enable-optimizations\
-  cd ~
-  sudo make
-  sudo make altinstall
-  sudo apt install python3-pip
-  sudo rm -rf Python-3.10.8 Python-3.10.8.tgz
-}
-
-function install_pip_packages() {
-  # add pip packages here
+function install_pip() {
+  sudo apt-get install -y python3-pip
+  pip3 install pynvim
 }
 
 function install_neovim() {
@@ -83,7 +70,7 @@ function install_neovim() {
   make CMAKE_BUILD_TYPE=Release
   sudo make install
   cd ~
-  sudo rm -rf neovim
+  sudo rm -rf neovim ~/.local/share/nvim
 }
 
 function install_packer() {
@@ -96,13 +83,13 @@ update
 install_apt_packages
 install_zsh
 install_cmake
-install_packer
 install_lazygit
 install_rust
-install_cargo_packages
 install_node
-install_npm_packages
+install_rust
+install_pip
 install_neovim
+install_packer
 
 # remove existing configurations
 rm -rf .gitconfig .tmux.conf .zshrc .p10k.zsh .config/nvim
@@ -112,4 +99,5 @@ ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
+[[ -d .config ]] || mkdir .config
 ln -s ~/.dotfiles/nvim ~/.config/nvim
