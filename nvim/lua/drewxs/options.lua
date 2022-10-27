b00273f30717b412ae042a89ad2257beb2ea707d
Andrew X. Shah <drewshah0@gmail.com>
2022-10-26 20:48:17 -0600

    feat: update vim opts

--- nvim/lua/drewxs/options.lua
+++ nvim/lua/drewxs/options.lua
@@ -2,9 +2,16 @@ vim.scriptencoding = "utf-8"
 vim.opt.encoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
 
-vim.opt.number = true
+vim.opt.shell = "zsh"
 vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.backspace = { "start", "eol", "indent" }
+vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
+vim.opt.inccommand = "split"
+vim.opt.background = "dark"
+vim.opt.wildoptions = "pum"
 vim.opt.title = true
+vim.opt.number = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.hlsearch = true
@@ -14,22 +21,16 @@ vim.opt.cmdheight = 1
 vim.opt.laststatus = 2
 vim.opt.expandtab = true
 vim.opt.scrolloff = 10
-vim.opt.shell = "zsh"
-vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
-vim.opt.inccommand = "split"
 vim.opt.ignorecase = true
 vim.opt.smarttab = true
 vim.opt.breakindent = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.wrap = false
-vim.opt.backspace = { "start", "eol", "indent" }
 vim.opt.cursorline = true
-vim.opt.winblend = 0
-vim.opt.wildoptions = "pum"
-vim.opt.pumblend = 5
-vim.opt.background = "dark"
 vim.opt.termguicolors = true
+vim.opt.winblend = 0
+vim.opt.pumblend = 5
 
 vim.opt.path:append({ "**" })
 vim.opt.wildignore:append({ "*/node_modules/*" })
