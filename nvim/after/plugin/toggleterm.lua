bf57a13198c443ba7ea0ecafb317ad844d27d532
Andrew X. Shah <drewshah0@gmail.com>
2022-10-30 22:58:44 -0600

    chore: update nvim config

--- nvim/after/plugin/toggleterm.lua
+++ nvim/after/plugin/toggleterm.lua
@@ -1,25 +1,26 @@
 require("toggleterm").setup({
 	size = 13,
 	open_mapping = [[<c-\>]],
-	shade_filetypes = {},
-	shade_terminals = true,
-	shading_factor = "1",
+	-- shade_filetypes = {},
+	-- shade_terminals = true,
+	-- shading_factor = "1",
 	start_in_insert = true,
 	persist_size = true,
 	direction = "horizontal",
+  autochdir = true,
 	highlights = {
 		-- highlights which map to a highlight group name and a table of it's values
 		-- NOTE: this is only a subset of values, any group placed here will be set for the terminal window split
-		Normal = {
-			guibg = "#000000",
-		},
-		NormalFloat = {
-			guibg = "#000000",
-		},
-		FloatBorder = {
-			guifg = "#000000",
-			guibg = "#000000",
-		},
+		-- Normal = {
+		-- 	guibg = "#000000",
+		-- },
+		-- NormalFloat = {
+		-- 	guibg = "#000000",
+		-- },
+		-- FloatBorder = {
+		-- 	guifg = "#000000",
+		-- 	guibg = "#000000",
+		-- },
 	},
 	auto_scroll = true,
 	float_opts = {
@@ -29,6 +30,6 @@ require("toggleterm").setup({
 		-- not natively supported but implemented in this plugin.
 		border = "curved",
 		-- like `size`, width and height can be a number or function which is passed the current terminal
-		winblend = 3,
+		winblend = 20,
 	},
 })
