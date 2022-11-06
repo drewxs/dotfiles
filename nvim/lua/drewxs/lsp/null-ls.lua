2eb4ca882e285b1a1a2418def62e45dfa35ca117
Andrew X. Shah <drewshah0@gmail.com>
2022-11-06 04:35:37 -0700

    feat: update null-ls config

--- nvim/lua/drewxs/lsp/null-ls.lua
+++ nvim/lua/drewxs/lsp/null-ls.lua
@@ -6,10 +6,9 @@ end
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
-		-- null_ls.builtins.diagnostics.eslint,
-		null_ls.builtins.code_actions.eslint_d,
-		-- null_ls.builtins.code_actions.ltrs,
 		null_ls.builtins.diagnostics.commitlint,
-		null_ls.builtins.diagnostics.cppcheck,
+		null_ls.builtins.code_actions.eslint_d,
+		-- null_ls.builtins.diagnostics.eslint,
+		-- null_ls.builtins.code_actions.ltrs,
 	},
 })
