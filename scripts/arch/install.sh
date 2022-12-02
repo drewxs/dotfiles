c55dccf2d0a0a1b5c0bfea690073918f10787fe4
Andrew X. Shah <drewshah0@gmail.com>
2022-12-02 09:13:24 -0700

    build: update arch install

--- scripts/arch/install.sh
+++ scripts/arch/install.sh
@@ -13,6 +13,7 @@ function update() {
 }
 
 function install_yay() {
+  package_exists yay && return
   cd ~
   sudo pacman -S --needed git base-devel --noconfirm
   git clone https://aur.archlinux.org/yay/git
@@ -25,34 +26,36 @@ function install_packages() {
   sudo pacman -S --noconfirm git neofetch neovim xclip ripgrep wget \
     xorg-xmodmap xorg-xev xorg-setxkbmap xorg-xset \
     rust nodejs npm python python-pip ruby go
-  sudo cargo install tree-sitter-cli stylua
-  sudo npm i -g neovim pnpm @fsouza/prettierd eslint_d typescript-language-server @commitlint/cli @commitlint/config-conventional
-  sudo gem install neovim shopify-cli
-  sudo pip install neovim
+  cargo install tree-sitter-cli stylua
+  npm i -g neovim pnpm @fsouza/prettierd eslint_d typescript-language-server @commitlint/cli @commitlint/config-conventional
+  gem install neovim shopify-cli
+  pip install neovim
 }
 
 function install_zsh_plugins() {
   sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
   sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
   sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+  package_exists zsh && return
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 }
 
 function install_snap() {
+  package_exists snap && return
   yay -S snapd
   sudo systemctl enable --now snapd.socket
   sudo systemctl enable --now snapd.apparmor
 }
 
 function remove_existing_configurations() {
-  cd ~
   sudo rm -rf ~/.cache/nvim ~/.config/nvim/plugin ~/.local/share/nvim
-  sudo rm -rf .gitconfig .tmux.conf .zshrc .p10k.zsh .config/nvim .config/xfce4/terminal/terminalrc 
+  sudo rm -rf ~/.gitconfig ~/.tmux.conf ~/.zshrc ~/.p10k.zsh ~/.config/nvim ~/.config/xfce4/terminal/terminalrc ~/.config/alacritty/alacritty.yml ~/.xinitrc
 }
 
 function create_symlinks() {
   ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-  ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
   ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
+  ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
   ln -s ~/.dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
   [[ -d .config ]] || mkdir .config
   ln -s ~/.dotfiles/nvim ~/.config/nvim
