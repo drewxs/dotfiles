1bafea4517a5322fd9dff3412c95c069410cd5e4
Andrew X. Shah <drewshah0@gmail.com>
2022-12-18 17:39:55 -0700

    feat: add highlight on yank with timeout

--- nvim/lua/base.lua
+++ nvim/lua/base.lua
@@ -13,5 +13,10 @@ vim.api.nvim_create_autocmd("InsertLeave", {
   command = "set nopaste",
 })
 
--- add asterisks in block comments
-vim.opt.formatoptions:append({ "r" })
+-- highlight yanked text
+vim.cmd([[
+  augroup highlight_yank
+  autocmd!
+  au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=100})
+  augroup END
+]])
