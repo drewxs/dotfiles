a6538a6173812a3bf97382df55b963d730380fbe
Andrew X. Shah <drewshah0@gmail.com>
2022-12-19 04:35:10 -0700

    chore: update options for latest onedarkpro version

--- nvim/after/plugin/themes/onedarkpro.rc.lua
+++ nvim/after/plugin/themes/onedarkpro.rc.lua
@@ -4,8 +4,6 @@ if not status then
 end
 
 onedarkpro.setup({
-  dark_theme = "onedark", -- The default dark theme
-  light_theme = "onelight", -- The default light theme
   colors = {}, -- Override default colors by specifying colors for 'onelight' or 'onedark' themes
   plugins = { -- Override which plugin highlight groups are loaded
     -- See the Supported Plugins section for a list of available plugins
@@ -26,6 +24,6 @@ onedarkpro.setup({
     cursorline = true, -- Use cursorline highlighting?
     transparency = true, -- Use a transparent background?
     terminal_colors = true, -- Use the colorscheme's colors for Neovim's :terminal?
-    window_unfocused_color = true, -- When the window is out of focus, change the normal background?
+    highlight_inactive_windows = true, -- When the window is out of focus, change the normal background?
   },
 })
