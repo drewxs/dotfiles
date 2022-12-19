b1adb752bc3eb09e34f92c7ca1d35bfed560b8c4
Andrew X. Shah <drewshah0@gmail.com>
2022-12-18 17:40:41 -0700

    chore: cleanup opts

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,43 +1,66 @@
+local opt = vim.opt
+local g = vim.g
+
+g.mapleader = " "
+
+-- encodinng
 vim.scriptencoding = "utf-8"
-vim.opt.encoding = "utf-8"
-vim.opt.fileencoding = "utf-8"
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
 
-vim.opt.shell = "zsh"
-vim.opt.mouse = "a"
-vim.opt.signcolumn = "number"
-vim.opt.clipboard = "unnamedplus"
-vim.opt.backspace = { "start", "eol", "indent" }
-vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
-vim.opt.inccommand = "split"
-vim.opt.background = "dark"
-vim.opt.wildoptions = "pum"
-vim.opt.whichwrap = "lh"
+-- general
+opt.shell = "zsh"
+opt.mouse = "a"
+opt.clipboard = "unnamedplus"
+opt.backspace = { "start", "eol", "indent" }
+opt.backupskip = { "/tmp/*", "/private/tmp/*" }
+opt.inccommand = "split"
+opt.wildoptions = "pum"
+opt.swapfile = false
+opt.formatoptions = "tcqjr"
 
-vim.opt.title = true
-vim.opt.number = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.hlsearch = true
-vim.opt.backup = false
-vim.opt.showcmd = true
-vim.opt.cmdheight = 1
-vim.opt.laststatus = 2
-vim.opt.expandtab = true
-vim.opt.scrolloff = 10
-vim.opt.ignorecase = true
-vim.opt.smarttab = true
-vim.opt.breakindent = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.wrap = false
-vim.opt.cursorline = true
-vim.opt.termguicolors = true
-vim.opt.winblend = 0
-vim.opt.pumblend = 5
+-- indentation
+opt.autoindent = true
+opt.smartindent = true
+opt.breakindent = true
+opt.expandtab = true
+opt.smarttab = true
+opt.shiftround = true
+opt.tabstop = 2
+opt.shiftwidth = 2
 
-vim.opt.path:append({ "**" })
-vim.opt.wildignore:append({ "*/node_modules/*" })
+-- search
+opt.hlsearch = true
+opt.ignorecase = true
+opt.incsearch = true
+opt.smartcase = true
 
-vim.opt.foldmethod = "expr"
-vim.opt.foldlevel = 99
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- interface
+opt.title = true
+opt.laststatus = 2
+opt.showcmd = true
+opt.showmode = false
+opt.cmdheight = 1
+opt.backup = false
+opt.scrolloff = 12
+opt.wrap = false
+opt.ruler = false
+opt.number = true
+opt.numberwidth = 2
+opt.background = "dark"
+opt.whichwrap = "lh"
+opt.signcolumn = "number"
+opt.cursorline = true
+opt.termguicolors = true
+opt.winblend = 0
+opt.pumblend = 5
+opt.timeoutlen = 100
+
+-- paths
+opt.path:append({ "**" })
+opt.wildignore:append({ "*/node_modules/*" })
+
+-- folds
+opt.foldmethod = "expr"
+opt.foldlevel = 99
+opt.foldexpr = "nvim_treesitter#foldexpr()"
