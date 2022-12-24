25bc31a77a432fddf186f0275bf17cdd5958c223
Andrew X. Shah <drewshah0@gmail.com>
2022-12-24 11:04:52 -0700

    fix(transparent): remove extra_groups

--- nvim/after/plugin/transparent.rc.lua
+++ nvim/after/plugin/transparent.rc.lua
@@ -1,14 +1,5 @@
 require("transparent").setup({
   enable = true, -- boolean: enable transparent
-  extra_groups = { -- table/string: additional groups that should be cleared
-    -- In particular, when you set it to 'all', that means all available groups
-    -- example of akinsho/nvim-bufferline.lua
-    "BufferLineTabClose",
-    "BufferlineBufferSelected",
-    "BufferLineFill",
-    "BufferLineBackground",
-    "BufferLineSeparator",
-    "BufferLineIndicatorSelected",
-  },
+  extra_groups = {}, -- table/string: additional groups that should be cleared
   exclude = {}, -- table: groups you don't want to clear
 })
