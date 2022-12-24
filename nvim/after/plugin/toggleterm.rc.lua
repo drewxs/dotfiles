302f936565148c0567f8f4ca4c7bd587d473d5d3
Andrew X. Shah <drewshah0@gmail.com>
2022-12-24 12:00:44 -0700

    toggleterm: disable shading

--- nvim/after/plugin/toggleterm.rc.lua
+++ nvim/after/plugin/toggleterm.rc.lua
@@ -2,7 +2,7 @@ require("toggleterm").setup({
   size = 13,
   open_mapping = [[<c-\>]],
   -- shade_filetypes = {},
-  -- shade_terminals = true,
+  shade_terminals = false,
   -- shading_factor = "1",
   start_in_insert = true,
   persist_size = true,
