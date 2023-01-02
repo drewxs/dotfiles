701afa31bfaa7893766a5e91d3732ef505deea8c
Andrew X. Shah <drewshah0@gmail.com>
2023-01-01 18:33:36 -0700

    nvim(transparent): add NvimTreeNormal to extra_groups

--- nvim/after/plugin/transparent.rc.lua
+++ nvim/after/plugin/transparent.rc.lua
@@ -1,5 +1,7 @@
 require("transparent").setup({
   enable = true, -- boolean: enable transparent
-  extra_groups = {}, -- table/string: additional groups that should be cleared
+  extra_groups = { -- table/string: additional groups that should be cleared
+    "NvimTreeNormal",
+  },
   exclude = {}, -- table: groups you don't want to clear
 })
