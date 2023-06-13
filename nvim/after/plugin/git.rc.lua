4a8011bda7c4fed919b20c863377c3e3ee5c0033
Andrew X. Shah <drewshah0@gmail.com>
2023-06-13 05:30:43 -0600

    nvim(plugins): rm git.nvim

--- nvim/after/plugin/git.rc.lua
+++
@@ -1,8 +0,0 @@
-require("git").setup({
-  keymaps = {
-    -- open blame window
-    blame = "<Leader>gb",
-    -- open file/folder in git repository
-    browse = "<Leader>go",
-  },
-})
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -99,7 +99,6 @@ require("packer").startup(function(use)
 
   -- git
   use("lewis6991/gitsigns.nvim")
-  use("dinhhuy258/git.nvim")
   use("kdheepak/lazygit.nvim")
 
   -- terminal
