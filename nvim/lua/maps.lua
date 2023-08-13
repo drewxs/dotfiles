6a9c82fbf3865b8890dcef83a92a9635ec463915
Andrew X. Shah <drewshah0@gmail.com>
2023-08-13 16:50:25 -0600

    nvim(maps): more perf map fn

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -1,8 +1,7 @@
 local map = function(mode, lhs, rhs, opts)
-  opts = opts or { silent = true, nowait = true }
-  for k, v in pairs(opts) do
-    opts[k] = v
-  end
+  local default_opts = { silent = true, nowait = true }
+  opts = opts or {}
+  setmetatable(opts, { __index = default_opts })
   vim.keymap.set(mode, lhs, rhs, opts)
 end
 
