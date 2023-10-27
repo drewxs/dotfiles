ba9018e6e84c159ba2527ea874b13d6fc0642637
Andrew X. Shah <drewshah0@gmail.com>
2023-10-27 15:03:09 -0600

    nvim(null-ls): rm code_actions diagnostics

--- nvim/after/plugin/lsp/null-ls.lua
+++ nvim/after/plugin/lsp/null-ls.lua
@@ -1,18 +1,9 @@
 local null_ls = require("null-ls")
 
-local code_actions = null_ls.builtins.code_actions
-local diagnostics = null_ls.builtins.diagnostics
 local formatting = null_ls.builtins.formatting
 
 null_ls.setup({
   sources = {
-    code_actions.eslint_d,
-    code_actions.shellcheck,
-
-    diagnostics.eslint_d,
-    diagnostics.shellcheck,
-    diagnostics.credo,
-
     formatting.black,
     formatting.prettierd,
     formatting.rustfmt,
