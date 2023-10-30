02eb31a8073ff67de7d1eaef9797928434d672b6
Andrew X. Shah <drewshah0@gmail.com>
2023-10-29 20:27:27 -0600

    nvim(autotag): set update_in_insert=true
    Issue with closing a tag causing lsp to update diagnostics incorrectly.

--- nvim/after/plugin/lsp/lspconfig.lua
+++ nvim/after/plugin/lsp/lspconfig.lua
@@ -160,3 +160,12 @@ nvim_lsp.clangd.setup({
     "--offset-encoding=utf-16",
   },
 })
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  underline = true,
+  virtual_text = {
+    spacing = 5,
+    severity_limit = "Warning",
+  },
+  update_in_insert = true,
+})
