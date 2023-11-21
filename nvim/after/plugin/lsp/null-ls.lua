54726ed994207ee4ad70f9c26c0c0055d5548dba
Andrew X. Shah <drewshah0@gmail.com>
2023-11-20 17:35:18 -0700

    nvim(null-ls): update sources

--- nvim/after/plugin/lsp/null-ls.lua
+++ nvim/after/plugin/lsp/null-ls.lua
@@ -2,12 +2,29 @@ local nls = require("null-ls")
 
 nls.setup({
   sources = {
+    nls.builtins.diagnostics.zsh,
+    nls.builtins.diagnostics.shellcheck,
+    nls.builtins.diagnostics.buf,
+    nls.builtins.diagnostics.eslint_d,
+    nls.builtins.diagnostics.credo,
+    nls.builtins.diagnostics.pylint,
+    nls.builtins.diagnostics.golangci_lint,
+    nls.builtins.diagnostics.rubocop,
+    nls.builtins.diagnostics.clang_check,
+    nls.builtins.diagnostics.luacheck,
+
+    nls.builtins.formatting.shfmt,
+    nls.builtins.formatting.buf,
     nls.builtins.formatting.black,
     nls.builtins.formatting.prettierd,
     nls.builtins.formatting.rustfmt,
-    nls.builtins.formatting.shfmt,
+    nls.builtins.formatting.leptosfmt,
     nls.builtins.formatting.stylua,
     nls.builtins.formatting.sql_formatter,
     nls.builtins.formatting.mix,
+    nls.builtins.formatting.surface,
+    nls.builtins.formatting.gofmt,
+    nls.builtins.formatting.rubyfmt,
+    nls.builtins.formatting.clang_format,
   },
 })
