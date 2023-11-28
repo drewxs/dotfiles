edb5ad94a001593557d514808b7630c37295fc67
Andrew X. Shah <drewshah0@gmail.com>
2023-11-27 20:58:03 -0700

    nvim(autocmds): resize splits on window resize

--- nvim/lua/base.lua
+++ nvim/lua/base.lua
@@ -5,13 +5,9 @@ vim.cmd([[colorscheme catppuccin]])
 vim.cmd([[let &t_Cs = '\e[4:3m']])
 vim.cmd([[let &t_Ce = '\e[4:0m']])
 
--- highlight yanked text
-vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-  pattern = "*",
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
+local function augroup(name)
+  return vim.api.nvim_create_augroup(name, { clear = true })
+end
 
 -- open tree on start
 vim.api.nvim_create_autocmd({ "VimEnter" }, {
@@ -20,14 +16,26 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, {
   end,
 })
 
--- auto refresh buffers on change
-vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
-  pattern = "*",
-  command = "if mode() != 'c' | checktime | endif",
+-- highlight yanked text
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+  group = augroup("highlight_yank"),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
 
--- turn off paste mode when leaving insert
-vim.api.nvim_create_autocmd({ "InsertLeave" }, {
-  pattern = "*",
-  command = "set nopaste",
+-- auto refresh on focus
+vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+  group = augroup("checktime"),
+  command = "checktime",
+})
+
+-- resize splits on window resize
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  group = augroup("resize_splits"),
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd("tabdo wincmd =")
+    vim.cmd("tabnext " .. current_tab)
+  end,
 })
