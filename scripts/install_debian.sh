570d2797f91ee658654c417f68eb8e8a5ac053d0
Andrew X. Shah <drewshah0@gmail.com>
2024-02-21 23:37:21 -0700

    scripts(install_debian): bump dotnet 7.0->8.0

--- scripts/install_debian.sh
+++ scripts/install_debian.sh
@@ -78,10 +78,7 @@ function install_go {
 
 function install_dotnet {
   echo "Installing dotnet..."
-  wget https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-  sudo dpkg -i packages-microsoft-prod.deb
-  rm packages-microsoft-prod.deb
-  sudo apt-get update && sudo apt-get install -y dotnet-sdk-7.0
+  sudo apt-get install -y dotnet-sdk-8.0
 }
 
 function install_neovim {
