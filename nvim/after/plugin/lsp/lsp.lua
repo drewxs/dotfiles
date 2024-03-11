a36e6fce4ec2b8e8ffce23f34be89a10044759aa
Andrew X. Shah <drewshah0@gmail.com>
2024-03-11 15:44:53 -0600

    nvim(lsp): enable update on insert

--- nvim/after/plugin/lsp/lsp.lua
+++ nvim/after/plugin/lsp/lsp.lua
@@ -65,3 +65,12 @@ vim.diagnostic.config({
   underline = true,
   severity_sort = true,
 })
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  underline = true,
+  virtual_text = {
+    spacing = 5,
+    severity_limit = "Warning",
+  },
+  update_in_insert = true,
+})
