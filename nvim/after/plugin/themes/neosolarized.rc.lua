578ac95163984dc9c80115a47a2c50e89cfe704e
Andrew X. Shah <drewshah0@gmail.com>
2024-04-01 03:12:50 -0600

    nvim(themes/neosolarized): disable bg

--- nvim/after/plugin/themes/neosolarized.rc.lua
+++ nvim/after/plugin/themes/neosolarized.rc.lua
@@ -1,4 +1,8 @@
-require("neosolarized").setup({
+local neosolarized = require("neosolarized")
+
+neosolarized.setup({
   comment_italics = true,
-  background_set = true,
+  background_set = false,
 })
+
+neosolarized.Group.link("WarningMsg", neosolarized.groups.Comment)
