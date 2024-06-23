afa2130d365cb8c4843cc31a210c2363c86ad6b0
Andrew X. Shah <drewshah0@gmail.com>
2024-06-23 11:41:34 -0600

    nvim(utils/map): allow passing table lhs

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -1,9 +1,21 @@
 local default_opts = { silent = true, nowait = true }
+local keymap_set = vim.keymap.set
+
+local function map(mode, lhs, rhs, opts)
+  opts = opts or {}
+  if not getmetatable(opts) then
+    setmetatable(opts, { __index = default_opts })
+  end
+
+  if type(lhs) == "table" then
+    for _, cmd in ipairs(lhs) do
+      keymap_set(mode, cmd, rhs, opts)
+    end
+  else
+    keymap_set(mode, lhs, rhs, opts)
+  end
+end
 
 return {
-  map = function(mode, lhs, rhs, opts)
-    opts = opts or {}
-    setmetatable(opts, { __index = default_opts })
-    vim.keymap.set(mode, lhs, rhs, opts)
-  end,
+  map = map,
 }
