8e5cf3c18dc68840b5e0dc8a545e020805f0fb70
Andrew X. Shah <drewshah0@gmail.com>
2024-06-24 23:44:45 -0600

    nvim(plugins): swap neosolarized

--- nvim/after/plugin/themes/neosolarized.rc.lua
+++
@@ -1,8 +0,0 @@
-local neosolarized = require("neosolarized")
-
-neosolarized.setup({
-  comment_italics = true,
-  background_set = false,
-})
-
-neosolarized.Group.link("WarningMsg", neosolarized.groups.Comment)
