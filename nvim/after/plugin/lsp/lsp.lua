a93ce865e211eded164aa5508bc2f81657c8d152
Andrew X. Shah <drewshah0@gmail.com>
2024-06-26 00:25:55 -0600

    nvim(deps): update

--- nvim/after/plugin/lsp/lsp.lua
+++ nvim/after/plugin/lsp/lsp.lua
@@ -86,9 +86,6 @@ vim.diagnostic.config({
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
   underline = true,
-  virtual_text = {
-    spacing = 5,
-    severity_limit = "Warning",
-  },
+  virtual_text = { spacing = 5 },
   update_in_insert = true,
 })
