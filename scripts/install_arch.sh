c73dd69bf805128c78ba933af7ab61121532689d
Andrew X. Shah <drewshah0@gmail.com>
2024-06-28 02:30:36 -0600

    pkg(arch): +google-java-format

--- scripts/install_arch.sh
+++ scripts/install_arch.sh
@@ -23,8 +23,9 @@ function install_sys_packages {
   sudo pacman -S --noconfirm \
     pacman-contrib wezterm tmux neofetch onefetch fzf wget xclip up ttf-sourcecodepro-nerd \
     docker docker-compose github-cli lazygit direnv python-distutils-extra thefuck \
-    tree-sitter shfmt shellcheck luarocks luacheck
-  yay -S --noconfirm google-chrome visual-studio-code-bin slack-desktop lazydocker gitmux fpp
+    tree-sitter shfmt shellcheck luarocks luacheck sqlfluff
+  yay -S --noconfirm google-chrome visual-studio-code-bin slack-desktop \
+    lazydocker gitmux fpp google-java-format
 }
 
 function install_languages {
