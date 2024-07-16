2b7c5762503cc06c719cd0f62e80bb5aaa69527a
Andrew X. Shah <drewshah0@gmail.com>
2024-07-16 10:11:41 -0600

    nvim(supermaven): set version before setup

--- nvim/after/plugin/lsp/supermaven.lua
+++ nvim/after/plugin/lsp/supermaven.lua
@@ -1,5 +1,10 @@
-require("supermaven-nvim").setup({
+local supermaven = require("supermaven-nvim")
+local api = require("supermaven-nvim").api
+
+supermaven.setup({
   keymaps = {
     accept_word = "<c-f>",
   },
 })
+
+api.use_free_version()
