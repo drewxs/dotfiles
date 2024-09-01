75d6ee57f643a1eedc6409f9e6ea2f4487496052
Andrew X. Shah <drewshah0@gmail.com>
2024-09-01 13:08:30 -0600

    nvim(which-key): migrate cfg v3

--- nvim/after/plugin/utils/which-key.rc.lua
+++ nvim/after/plugin/utils/which-key.rc.lua
@@ -1,192 +1,106 @@
 local which_key = require("which-key")
 local is_open = require("utils").is_open
 
-local setup = {
-  plugins = {
-    marks = true, -- shows a list of your marks on ' and `
-    registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
-    spelling = {
-      enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-      suggestions = 20, -- how many suggestions should be shown in the list?
-    },
-    -- the presets plugin, adds help for a bunch of default keybindings in Neovim
-    -- No actual key bindings are created
-    presets = {
-      operators = true, -- adds help for operators like d, y, ... and registers them for motion / text object completion
-      motions = true, -- adds help for motions
-      text_objects = true, -- help for text objects triggered after entering an operator
-      windows = false, -- default bindings on <c-w>
-      nav = true, -- misc bindings to work with windows
-      z = true, -- bindings for folds, spelling and others prefixed with z
-      g = true, -- bindings for prefixed with g
-    },
-  },
-  -- add operators that will trigger motion and text object completion
-  -- to enable all native operators, set the preset / operators plugin above
-  -- operators = { gc = "Comments" },
-  -- key_labels = {
-  -- override the label used to display some keys. It doesn't effect WK in any other way.
-  -- For example:
-  -- ["<space>"] = "SPC",
-  -- ["<cr>"] = "RET",
-  -- ["<tab>"] = "TAB",
-  -- },
-  icons = {
-    breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-    separator = "➜", -- symbol used between a key and it's label
-    group = "+", -- symbol prepended to a group
-  },
-  popup_mappings = {
-    scroll_down = "<c-d>", -- binding to scroll down inside the popup
-    scroll_up = "<c-u>", -- binding to scroll up inside the popup
-  },
-  window = {
-    border = "rounded", -- none, single, double, shadow
-    position = "bottom", -- bottom, top
-    margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-    padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-    winblend = 20,
-  },
-  layout = {
-    height = { min = 4, max = 25 }, -- min and max height of the columns
-    width = { min = 20, max = 50 }, -- min and max width of the columns
-    spacing = 3, -- spacing between columns
-    align = "left", -- align columns left, center or right
-  },
-  ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
-  hidden = { "<silent>", "<cmd>", "<cr>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
-  show_help = true, -- show help message on the command line when the popup is visible
-  triggers = "auto", -- automatically setup triggers
-  -- triggers = {"<leader>"} -- or specify a list manually
-  triggers_blacklist = {
-    -- list of mode / prefixes that should never be hooked by WhichKey
-    -- this is mostly relevant for key maps that start with a native binding
-    -- most people should not need to change this
-    i = { "j", "k" },
-    v = { "j", "k" },
-  },
-}
+which_key.setup()
+which_key.add({
+  { "<leader>c",  "<cmd>Bdelete!<cr>",                                 desc = "Close" },
+  { "<leader>e",  "<cmd>NvimTreeToggle<cr>",                           desc = "Explorer" },
+  { "<leader>q",  "<cmd>q!<cr>",                                       desc = "Quit" },
+  { "<leader>w",  "<cmd>w!<cr>",                                       desc = "Save" },
 
-local opts = {
-  mode = "n", -- NORMAL mode
-  prefix = "<leader>",
-  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-  silent = true, -- use `silent` when creating keymaps
-  noremap = true, -- use `noremap` when creating keymaps
-  nowait = true, -- use `nowait` when creating keymaps
-}
+  { "<leader>d",  group = "Debug" },
+  { "<leader>dc", "<cmd>lua require 'dap'.continue()<cr>",             desc = "Continue" },
+  { "<leader>dd", "<cmd>lua require 'dap'.toggle_breakpoint()<cr>",    desc = "Toggle Breakpoint" },
+  { "<leader>di", "<cmd>lua require 'dap'.step_into()<cr>",            desc = "Step Into" },
+  { "<leader>do", "<cmd>lua require 'dap'.step_over()<cr>",            desc = "Step Over" },
+  { "<leader>dr", "<cmd>lua require 'dap'.repl.open()<cr>",            desc = "REPL" },
 
-local mappings = {
-  c = { "<cmd>Bdelete!<cr>", "Close" },
-  e = { "<cmd>NvimTreeToggle<cr>", "Explorer" },
-  q = { "<cmd>q!<cr>", "Quit" },
-  w = { "<cmd>w!<cr>", "Save" },
-  d = {
-    name = "Debug",
-    d = { "<cmd>lua require 'dap'.toggle_breakpoint()<cr>", "Toggle Breakpoint" },
-    c = { "<cmd>lua require 'dap'.continue()<cr>", "Continue" },
-    o = { "<cmd>lua require 'dap'.step_over()<cr>", "Step Over" },
-    i = { "<cmd>lua require 'dap'.step_into()<cr>", "Step Into" },
-    r = { "<cmd>lua require 'dap'.repl.open()<cr>", "REPL" },
-  },
-  g = {
-    name = "Git",
-    g = { "<cmd>LazyGit<cr>", "Lazygit" },
-    j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
-    k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
-    l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
-    p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
-    r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
-    R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
-    s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
-    u = { "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>", "Undo Stage Hunk" },
-    b = { "<cmd>Telescope git_branches<cr>", "Checkout Branch" },
-    c = { "<cmd>Telescope git_commits<cr>", "Checkout Commit" },
-    d = { "<cmd>Gitsigns diffthis HEAD<cr>", "Diff" },
-  },
-  l = {
-    name = "LSP",
-    d = { "<cmd>Telescope lsp_document_diagnostics<cr>", "Document Diagnostics" },
-    w = { "<cmd>Telescope lsp_workspace_diagnostics<cr>", "Workspace Diagnostics" },
-    f = { "<cmd>lua vim.lsp.buf.format{async=true}<cr>", "Format" },
-    i = { "<cmd>LspInfo<cr>", "Info" },
-    I = { "<cmd>LspInstallInfo<cr>", "Installer Info" },
-    m = { "<cmd>messages<cr>", "Messages" },
-    q = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Quickfix" },
-    s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
-    S = { "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", "Workspace Symbols" },
-    t = { "<cmd>SupermavenToggle<cr>", "Toggle Supermaven" },
-  },
-  m = {
-    name = "Mason",
-    m = { "<cmd>Mason<cr>", "GUI" },
-    u = { "<cmd>MasonUpdate<cr>", "Update" },
-    l = { "<cmd>MasonLog<cr>", "Log" },
-  },
-  p = {
-    name = "Plugins",
-    c = { "<cmd>Lazy clean<cr>", "Clean" },
-    h = { "<cmd>Lazy health<cr>", "Health Check" },
-    i = { "<cmd>Lazy install<cr>", "Install" },
-    p = { "<cmd>Lazy<cr>", "GUI" },
-    r = { "<cmd>Lazy restore<cr>", "Restore" },
-    s = { "<cmd>Lazy sync<cr>", "Sync" },
-    u = { "<cmd>Lazy update<cr>", "Update" },
-  },
-  s = {
-    name = "Search",
-    b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
-    c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
-    h = { "<cmd>Telescope help_tags<cr>", "Find Help" },
-    m = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
-    r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
-    R = { "<cmd>Telescope registers<cr>", "Registers" },
-    k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
-    C = { "<cmd>Telescope commands<cr>", "Commands" },
-  },
-  t = {
-    name = "Treesitter",
-    u = { "<cmd>TSUpdate<cr>", "Update" },
-  },
-  u = {
-    name = "Utilities",
-    x = { "<cmd>!chmod +x %<cr>", "Make Executable" },
-    r = {
-      function()
-        local api = require("nvim-tree.api")
-        local node = api.tree.get_node_under_cursor()
-        if node and node.absolute_path then
-          vim.cmd("term " .. node.absolute_path)
-        else
-          print("No file selected")
-        end
-      end,
-      "Run Shell Script",
-    },
-    m = { "<cmd>MarkdownPreview<cr>", "Markdown Preview" },
-  },
-  x = {
-    name = "Terminal",
-    f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
-    h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
-    v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
-    x = {
-      function()
-        vim.cmd("ToggleTerm direction=horizontal")
+  { "<leader>g",  group = "Git" },
+  { "<leader>gg", "<cmd>LazyGit<cr>",                                  desc = "Lazygit" },
+  { "<leader>gd", "<cmd>lua require 'gitsigns'.diffthis('~')<cr>",     desc = "Diff" },
+  { "<leader>gj", "<cmd>lua require 'gitsigns'.next_hunk()<cr>",       desc = "Next Hunk" },
+  { "<leader>gk", "<cmd>lua require 'gitsigns'.prev_hunk()<cr>",       desc = "Prev Hunk" },
+  { "<leader>gl", "<cmd>lua require 'gitsigns'.blame_line()<cr>",      desc = "Blame" },
+  { "<leader>gp", "<cmd>lua require 'gitsigns'.preview_hunk()<cr>",    desc = "Preview Hunk" },
+  { "<leader>gr", "<cmd>lua require 'gitsigns'.reset_hunk()<cr>",      desc = "Reset Hunk" },
+  { "<leader>gR", "<cmd>lua require 'gitsigns'.reset_buffer()<cr>",    desc = "Reset Buffer" },
+  { "<leader>gs", "<cmd>lua require 'gitsigns'.stage_hunk()<cr>",      desc = "Stage Hunk" },
+  { "<leader>gu", "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>", desc = "Undo Stage Hunk" },
 
-        if is_open("NvimTree") then
-          vim.cmd("NvimTreeClose")
-          vim.cmd("NvimTreeOpen")
-        end
+  { "<leader>l",  group = "LSP" },
+  { "<leader>lI", "<cmd>LspInstallInfo<cr>",                           desc = "Installer Info" },
+  { "<leader>lS", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",  desc = "Workspace Symbols" },
+  { "<leader>ld", "<cmd>Telescope lsp_document_diagnostics<cr>",       desc = "Document Diagnostics" },
+  { "<leader>lf", "<cmd>lua vim.lsp.buf.format{async=true}<cr>",       desc = "Format" },
+  { "<leader>li", "<cmd>LspInfo<cr>",                                  desc = "Info" },
+  { "<leader>lm", "<cmd>messages<cr>",                                 desc = "Messages" },
+  { "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<cr>",          desc = "Quickfix" },
+  { "<leader>ls", "<cmd>Telescope lsp_document_symbols<cr>",           desc = "Document Symbols" },
+  { "<leader>lt", "<cmd>SupermavenToggle<cr>",                         desc = "Toggle Supermaven" },
+  { "<leader>lw", "<cmd>Telescope lsp_workspace_diagnostics<cr>",      desc = "Workspace Diagnostics" },
 
-        vim.cmd("wincmd l")
-        vim.cmd("wincmd j")
-        vim.cmd("startinsert")
-      end,
-      "Toggle",
-    },
+  { "<leader>m",  group = "Mason" },
+  { "<leader>ml", "<cmd>MasonLog<cr>",                                 desc = "Log" },
+  { "<leader>mm", "<cmd>Mason<cr>",                                    desc = "GUI" },
+  { "<leader>mu", "<cmd>MasonUpdate<cr>",                              desc = "Update" },
+
+  { "<leader>p",  group = "Plugins" },
+  { "<leader>pc", "<cmd>Lazy clean<cr>",                               desc = "Clean" },
+  { "<leader>ph", "<cmd>Lazy health<cr>",                              desc = "Health Check" },
+  { "<leader>pi", "<cmd>Lazy install<cr>",                             desc = "Install" },
+  { "<leader>pp", "<cmd>Lazy<cr>",                                     desc = "GUI" },
+  { "<leader>pr", "<cmd>Lazy restore<cr>",                             desc = "Restore" },
+  { "<leader>ps", "<cmd>Lazy sync<cr>",                                desc = "Sync" },
+  { "<leader>pu", "<cmd>Lazy update<cr>",                              desc = "Update" },
+
+  { "<leader>s",  group = "Search" },
+  { "<leader>sC", "<cmd>Telescope commands<cr>",                       desc = "Commands" },
+  { "<leader>sR", "<cmd>Telescope registers<cr>",                      desc = "Registers" },
+  { "<leader>sb", "<cmd>Telescope git_branches<cr>",                   desc = "Checkout branch" },
+  { "<leader>sc", "<cmd>Telescope colorscheme<cr>",                    desc = "Colorscheme" },
+  { "<leader>sh", "<cmd>Telescope help_tags<cr>",                      desc = "Find Help" },
+  { "<leader>sk", "<cmd>Telescope keymaps<cr>",                        desc = "Keymaps" },
+  { "<leader>sm", "<cmd>Telescope man_pages<cr>",                      desc = "Man Pages" },
+  { "<leader>sr", "<cmd>Telescope oldfiles<cr>",                       desc = "Open Recent File" },
+
+  { "<leader>t",  group = "Treesitter" },
+  { "<leader>tu", "<cmd>TSUpdate<cr>",                                 desc = "Update" },
+
+  { "<leader>u",  group = "Utilities" },
+  { "<leader>um", "<cmd>MarkdownPreview<cr>",                          desc = "Markdown Preview" },
+  {
+    "<leader>ur",
+    function()
+      local api = require("nvim-tree.api")
+      local node = api.tree.get_node_under_cursor()
+      if node and node.absolute_path then
+        vim.cmd("term " .. node.absolute_path)
+      else
+        print("No file selected")
+      end
+    end,
+    desc = "Run Shell Script",
   },
-}
+  { "<leader>ux", "<cmd>!chmod +x %<cr>",                             desc = "Make Executable" },
 
-which_key.setup(setup)
-which_key.register(mappings, opts)
+  { "<leader>x",  group = "Terminal" },
+  { "<leader>xf", "<cmd>ToggleTerm direction=float<cr>",              desc = "Float" },
+  { "<leader>xh", "<cmd>ToggleTerm size=10 direction=horizontal<cr>", desc = "Horizontal" },
+  { "<leader>xv", "<cmd>ToggleTerm size=80 direction=vertical<cr>",   desc = "Vertical" },
+  {
+    "<leader>xx",
+    function()
+      vim.cmd("ToggleTerm direction=horizontal")
+
+      if is_open("NvimTree") then
+        vim.cmd("NvimTreeClose")
+        vim.cmd("NvimTreeOpen")
+      end
+
+      vim.cmd("wincmd l")
+      vim.cmd("wincmd j")
+      vim.cmd("startinsert")
+    end,
+    desc = "Toggle",
+  },
+})
