e8f378b27f4787040483fcd999799cb1ea113ab7
Andrew X. Shah <drew@drewxs.dev>
2024-11-21 18:07:36 -0700

    nvim(deps): update

--- nvim/after/plugin/lsp/lsp.lua
+++ nvim/after/plugin/lsp/lsp.lua
@@ -1,52 +1,58 @@
-local lsp = require("lsp-zero").preset({})
-local nvim_lsp = require("lspconfig")
+local lsp = require("lsp-zero")
+local lspconfig = require("lspconfig")
 local mason = require("mason")
 local mason_lspconfig = require("mason-lspconfig")
 local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
+local lspconfig_defaults = lspconfig.util.default_config
+lspconfig_defaults.capabilities =
+  vim.tbl_deep_extend("force", lspconfig_defaults.capabilities, cmp_nvim_lsp.default_capabilities())
+
+local servers = {
+  "bashls",
+  "rust_analyzer",
+  "clangd",
+  "omnisharp",
+  "gopls",
+  "jdtls",
+  "lua_ls",
+  "elixirls",
+  "pyright",
+  "solargraph",
+  "tsserver",
+  "eslint",
+  "html",
+  "cssls",
+  "cssmodules_ls",
+  "tailwindcss",
+  "jsonls",
+  "graphql",
+  "sqlls",
+  "prismals",
+  "dockerls",
+  "docker_compose_language_service",
+  "terraformls",
+  "marksman",
+  "taplo",
+  "buf_ls",
+  "autotools_ls",
+}
+
 mason.setup()
 mason_lspconfig.setup({
-  ensure_installed = {
-    "bashls",
-    "rust_analyzer",
-    "clangd",
-    "omnisharp",
-    "gopls",
-    "jdtls",
-    "lua_ls",
-    "elixirls",
-    "pyright",
-    "solargraph",
-    "tsserver",
-    "eslint",
-    "html",
-    "cssls",
-    "cssmodules_ls",
-    "tailwindcss",
-    "jsonls",
-    "graphql",
-    "sqlls",
-    "prismals",
-    "dockerls",
-    "docker_compose_language_service",
-    "terraformls",
-    "marksman",
-    "taplo",
-    "buf_ls",
-    "autotools_ls",
-  },
+  ensure_installed = servers,
   automatic_installation = true,
   handlers = { lsp.default_setup },
 })
 
-local capabilities = cmp_nvim_lsp.default_capabilities()
 local installed_servers = mason_lspconfig.get_installed_servers()
+local capabilities = cmp_nvim_lsp.default_capabilities()
 
 for _, server in ipairs(installed_servers) do
-  nvim_lsp[server].setup({ capabilities })
+  lspconfig[server].setup({ capabilities })
 end
 
-nvim_lsp.lua_ls.setup({
+lspconfig.lua_ls.setup({
   settings = {
     Lua = {
       diagnostics = {
@@ -56,25 +62,23 @@ nvim_lsp.lua_ls.setup({
   },
 })
 
-nvim_lsp.rust_analyzer.setup({
+lspconfig.rust_analyzer.setup({
   on_attach = function(_, bufnr)
     vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
   end,
 })
 
-nvim_lsp.clangd.setup({
+lspconfig.clangd.setup({
   cmd = { "clangd", "--offset-encoding=utf-16" },
 })
 
-nvim_lsp.tailwindcss.setup({
+lspconfig.tailwindcss.setup({
   init_options = {
     userLanguages = {
       rust = "html",
     },
   },
 })
-
-lsp.setup()
 
 vim.diagnostic.config({
   virtual_text = true,
