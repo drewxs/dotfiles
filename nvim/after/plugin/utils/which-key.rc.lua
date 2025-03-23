43dd7e9e2e32d06e516589d95acfb674855cc39c
Andrew X. Shah <drew@drewxs.dev>
2025-03-22 19:07:50 -0600

    nvim(wk): git_bcommits telescope->fzflua

--- nvim/after/plugin/utils/which-key.rc.lua
+++ nvim/after/plugin/utils/which-key.rc.lua
@@ -29,7 +29,7 @@ which_key.add({
   { "<leader>g", group = "Git" },
   { "<leader>gg", "<cmd>LazyGit<cr>", desc = "Lazygit" },
   { "<leader>gd", "<cmd>lua require 'gitsigns'.diffthis('~')<cr>", desc = "Diff" },
-  { "<leader>gh", "<cmd>Telescope git_bcommits<cr>", desc = "File History" },
+  { "<leader>gh", "<cmd>FzfLua git_bcommits<cr>", desc = "File History" },
   { "<leader>gj", "<cmd>lua require 'gitsigns'.next_hunk()<cr>", desc = "Next Hunk" },
   { "<leader>gk", "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", desc = "Prev Hunk" },
   { "<leader>gl", "<cmd>lua require 'gitsigns'.blame_line()<cr>", desc = "Blame" },
