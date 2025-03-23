61df15ebd20a44c1e129a9cf447e0c3d606b6f65
Andrew X. Shah <drew@drewxs.dev>
2025-03-22 19:45:37 -0600

    nvim(fzf-lua): +cfg

---
+++ nvim/after/plugin/utils/fzf-lua.rc.lua
@@ -0,0 +1,78 @@
+local fzf_lua = require("fzf-lua")
+
+local utils = fzf_lua.utils
+local actions = fzf_lua.actions
+
+local function hl_validate(hl)
+  return not utils.is_hl_cleared(hl) and hl or nil
+end
+
+fzf_lua.setup({
+  desc = "match telescope default highlights|keybinds",
+  fzf_opts = { ["--layout"] = "default", ["--marker"] = "+" },
+  winopts = {
+    width = 0.8,
+    height = 0.9,
+    preview = {
+      hidden = false,
+    },
+  },
+  hls = {
+    normal = hl_validate("TelescopeNormal"),
+    border = hl_validate("TelescopeBorder"),
+    title = hl_validate("TelescopePromptTitle"),
+    help_normal = hl_validate("TelescopeNormal"),
+    help_border = hl_validate("TelescopeBorder"),
+    preview_normal = hl_validate("TelescopeNormal"),
+    preview_border = hl_validate("TelescopeBorder"),
+    preview_title = hl_validate("TelescopePreviewTitle"),
+    -- builtin preview only
+    cursor = hl_validate("Cursor"),
+    cursorline = hl_validate("TelescopeSelection"),
+    cursorlinenr = hl_validate("TelescopeSelection"),
+    search = hl_validate("IncSearch"),
+  },
+  fzf_colors = {
+    ["fg"] = { "fg", "TelescopeNormal" },
+    ["bg"] = { "bg", "TelescopeNormal" },
+    ["hl"] = { "fg", "TelescopeMatching" },
+    ["fg+"] = { "fg", "TelescopeSelection" },
+    ["bg+"] = { "bg", "TelescopeSelection" },
+    ["hl+"] = { "fg", "TelescopeMatching" },
+    ["info"] = { "fg", "TelescopeMultiSelection" },
+    ["border"] = { "fg", "TelescopeBorder" },
+    ["gutter"] = "-1",
+    ["query"] = { "fg", "TelescopePromptNormal" },
+    ["prompt"] = { "fg", "TelescopePromptPrefix" },
+    ["pointer"] = { "fg", "TelescopeSelectionCaret" },
+    ["marker"] = { "fg", "TelescopeSelectionCaret" },
+    ["header"] = { "fg", "TelescopeTitle" },
+  },
+  keymap = {
+    builtin = {
+      true,
+      ["<C-d>"] = "preview-page-down",
+      ["<C-u>"] = "preview-page-up",
+    },
+    fzf = {
+      true,
+      ["ctrl-d"] = "preview-page-down",
+      ["ctrl-u"] = "preview-page-up",
+      ["ctrl-q"] = "select-all+accept",
+    },
+  },
+  actions = {
+    files = {
+      ["enter"] = actions.file_edit_or_qf,
+      ["ctrl-x"] = actions.file_split,
+      ["ctrl-v"] = actions.file_vsplit,
+      ["ctrl-t"] = actions.file_tabedit,
+      ["alt-q"] = actions.file_sel_to_qf,
+    },
+  },
+  buffers = {
+    keymap = { builtin = { ["<C-d>"] = false } },
+    actions = { ["ctrl-x"] = false, ["ctrl-d"] = { actions.buf_del, actions.resume } },
+  },
+  defaults = { git_icons = false },
+})
